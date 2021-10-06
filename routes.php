81479135035ec6251f34b7e9be6ed360bf74de8f
Vlahy <73841311+Vlahy@users.noreply.github.com>
2021-10-06 09:59:23 +0200

    Changed routes, used Bramus $route->mount() method.

--- routes.php
+++ routes.php
@@ -8,6 +8,12 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
 
 require __DIR__ . '/vendor/autoload.php';
 
+use Api\Intern\Create as InternCreate;
+use Api\Intern\Read as InternRead;
+use Api\Intern\Delete as InternDelete;
+use Api\Mentor\Read as MentorRead;
+use Api\Mentor\Delete as MentorDelete;
+use Api\Group\Read as GroupRead;
 use Bramus\Router\Router;
 
 $router = new Router();
@@ -35,31 +41,63 @@ $router->get('/', function () {
     //header('Location: /src/Api/Intern/Read.php');
 });
 
-//Route for showing intern data
-$readIntern = new \Api\Intern\Read();
-$router->get('/intern/(\w+)', function ($id) use ($readIntern) {
-    $readIntern->read($id);
+//Defining routes for handling HTTP Request methods for Intern
+$router->mount('/intern', function () use ($router){
+
+    //Instantiating classes for Intern
+    $insertIntern = new InternCreate();
+    $readIntern = new InternRead();
+    $deleteIntern = new InternDelete();
+
+    //Route for getting Intern data
+    $router->get('/(\w+)', function ($id) use ($readIntern) {
+        $readIntern->read($id);
+    });
+
+    //Route for creating intern
+    $router->post('', function () use ($insertIntern) {
+        $insertIntern->create();
+    });
+
+    //Route for deleting intern from Database
+    $router->delete('/(\w+)', function ($id) use ($deleteIntern) {
+        $deleteIntern->delete($id);
+    });
 });
 
-$deleteIntern = new \Api\Intern\Delete();
-$router->delete('/intern/(\w+)', function ($id) use ($deleteIntern) {
-   $deleteIntern->delete($id);
+//Defining routes for handling HTTP Request methods for Mentor
+$router->mount('/mentor', function () use ($router) {
+
+    //Instantiating classes for Mentor
+    $readMentor = new MentorRead();
+    $deleteMentor = new MentorDelete();
+
+    //Route for getting Mentor data
+    $router->get('/(\w+)', function ($id) use ($readMentor) {
+        $readMentor->read($id);
+    });
+
+    //Route for deleting Mentor from Database
+    $router->delete('/(\w+)', function ($id) use ($deleteMentor) {
+        $deleteMentor->delete($id);
+    });
 });
 
-$readMentor = new \Api\Mentor\Read();
-$router->get('/mentor/(\w+)', function ($id) use ($readMentor) {
-    $readMentor->read($id);
+//Defining routes for handling HTTP Request methods for Group
+$router->mount('/group', function () use ($router){
+
+    //Instantiating classes for Group
+    $readGroup = new GroupRead();
+
+    //Route for getting Group data
+    $router->get('/(\w+)', function ($id) use ($readGroup) {
+        echo ($readGroup->read($id));
+    });
+
+
+
 });
 
-$deleteMentor = new \Api\Intern\Delete();
-$router->delete('/intern/(\w+)', function ($id) use ($deleteMentor) {
-    $deleteMentor->delete($id);
-});
-
-$readGroup = new \Api\Group\Read();
-$router->get('/group/(\w+)', function ($id) use ($readGroup) {
-    echo ($readGroup->read($id));
-});
 
 
 $router->run();
