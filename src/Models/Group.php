328566b38d330df528dcabdb3ec84613c4098722
Vlahy <73841311+Vlahy@users.noreply.github.com>
2021-10-08 11:16:58 +0200

    Added delete endpoint for group.

--- src/Models/Group.php
+++ src/Models/Group.php
@@ -15,7 +15,7 @@ class Group implements CrudInterface
         $this->conn = $db;
     }
 
-    public function create()
+    public function create(): bool
     {
         $query = "INSERT INTO `group` SET group_name = :group_name";
         $stmt = $this->conn->prepare($query);
@@ -44,8 +44,32 @@ class Group implements CrudInterface
         // TODO: Implement update() method.
     }
 
-    public function delete($id)
+    public function delete($id): bool
     {
-        // TODO: Implement delete() method.
+        $test = $this->conn->prepare("SELECT group_id FROM `group` g WHERE g.group_id ='" . $id . "'");
+        $test->execute();
+        $test->fetchAll();
+
+        if ($test->rowCount() > 0) {
+
+            $query = "DELETE FROM `group` g WHERE g.group_id = :id";
+            $stmt1 = $this->conn->prepare($query);
+
+            $testIntern = $this->conn->prepare("SELECT group_id FROM intern i WHERE i.group_id ='" . $id . "'");
+            $testIntern->execute();
+            $testIntern->fetchAll();
+
+            if($testIntern->rowCount() > 0){
+                $queryUpdate = "UPDATE intern i SET i.group_id = null WHERE i.group_id = :id";
+                $stmt2 = $this->conn->prepare($queryUpdate);
+                $stmt1->execute(['id' => $id]);
+                $stmt2->execute(['id' => $id]);
+                return true;
+            }else {
+                $stmt1->execute(['id' => $id]);
+                return true;
+            }
+        }
+        return false;
     }
 }
